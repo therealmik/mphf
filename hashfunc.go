dc98ac0f80ac8fb2d33d205a516c12a15a97edcf
Michael Samuel <mik@miknet.net>
2014-11-13 16:23:39 +1100

    Added BigSum() which uses siphash 128-bit tags

--- hashfunc.go
+++ hashfunc.go
@@ -29,3 +29,15 @@ func (self *HashFunc) Sum(data []byte) (ret [NUM_HASHES]uint) {
 	return ret
 }
 
+// Note: assumes M < (1 << 42)
+func (self *HashFunc) BigSum(data []byte) (ret [NUM_HASHES]uint64) {
+	h0, h1 := siphash.Hash128(self.K0, self.K1, data)
+	ret[0] = h0 % self.M
+	ret[1] = h1 % self.M
+
+	upper0, upper1 := h0 >> 42, h1 >> 42
+	ret[2] = (upper0 | (upper1 << 22)) % self.M
+
+	return ret
+}
+
